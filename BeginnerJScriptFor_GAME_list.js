96ee227f80aebdc63d013743b4e1ce90e8fdcc98
Aidan <55807185+AtomicOlsen@users.noreply.github.com>
2021-10-20 14:43:37 -0400

    Messed with broken button more

--- BeginnerJScriptFor_GAME_list.js
+++ BeginnerJScriptFor_GAME_list.js
@@ -2,9 +2,10 @@ window.addEventListener('DOMContentLoaded',init,false);
             
 function init() {
     alert('The page loaded!');
-    var buttons = document.getElementsByTagName("button")
-    button[0].addEventListener('click', HideSSB,false)
+    var button = document.getElementsByClassName("button")
 }
+
+button.addEventListener('click', HideSSB,false)
 
 function HideSSB(){
     document.getElementsByClassName('SSB')[0].style.visibility='hidden';
