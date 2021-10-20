1899a0cb804b25dfb7529935bc64495469ae0ee2
Aidan <55807185+AtomicOlsen@users.noreply.github.com>
2021-10-20 15:15:38 -0400

    added working hide button to Games html page

--- BeginnerJScriptFor_GAME_list.js
+++ BeginnerJScriptFor_GAME_list.js
@@ -1,13 +1,14 @@
 window.addEventListener('DOMContentLoaded',init,false);
             
 function init() {
-    alert('The page loaded!');
     var button = document.getElementsByClassName("button")
+    button[0].addEventListener('click',HideSSB,false)
 }
 
-button.addEventListener('click', HideSSB,false)
-
 function HideSSB(){
-    document.getElementsByClassName('SSB')[0].style.visibility='hidden';
-    
+    var SSB = document.getElementsByClassName('SSB')
+    for (var i = 0, length = SSB.length; i < length; i++) 
+    SSB[i].style.visibility='hidden';
+
+
 }
