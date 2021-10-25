33b59c5b7114634ce2220d053d322470e72fa6b3
Aidan <55807185+AtomicOlsen@users.noreply.github.com>
2021-10-25 12:40:41 -0400

    Added 5 total buttons that hide game name to allow identification of what games characters are in. Updated JScript file to reflect those functions.

--- BeginnerJScriptFor_GAME_list.js
+++ BeginnerJScriptFor_GAME_list.js
@@ -4,6 +4,9 @@ function init() {
     var button = document.getElementsByClassName("button")
     button[0].addEventListener('click',HideSSB,false)
     button[1].addEventListener('click',HideSSB2,false)
+    button[2].addEventListener('click',HideSSB3,false)
+    button[3].addEventListener('click',HideSSB4,false)
+    button[4].addEventListener('click',HideSSB5,false)
 }
 
 function HideSSB(){
@@ -14,9 +17,30 @@ function HideSSB(){
 
 }
 function HideSSB2(){
-    var SSB2 = document.getElementsByClassName('SSBB')
+    var SSB2 = document.getElementsByClassName('SSBM')
     for (var i = 0, length = SSB2.length; i < length; i++) 
-    SSBB[i].style.visibility='hidden';
+    SSB2[i].style.visibility='hidden';
+
+
+}
+function HideSSB3(){
+    var SSB3 = document.getElementsByClassName('SSBB')
+    for (var i = 0, length = SSB3.length; i < length; i++) 
+    SSB3[i].style.visibility='hidden';
+
+
+}
+function HideSSB4(){
+    var SSB4 = document.getElementsByClassName('SSB4')
+    for (var i = 0, length = SSB4.length; i < length; i++) 
+    SSB4[i].style.visibility='hidden';
+
+
+}
+function HideSSB5(){
+    var SSB5 = document.getElementsByClassName('SSBU')
+    for (var i = 0, length = SSB5.length; i < length; i++) 
+    SSB5[i].style.visibility='hidden';
 
 
 }
