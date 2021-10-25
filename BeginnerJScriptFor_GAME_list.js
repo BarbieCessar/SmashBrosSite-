c4b39546ea62fd68a4062ad42d780028dccab5f8
Aidan <55807185+AtomicOlsen@users.noreply.github.com>
2021-10-25 12:25:17 -0400

    fixed order of one entry wrong in gamelist.html

--- BeginnerJScriptFor_GAME_list.js
+++ BeginnerJScriptFor_GAME_list.js
@@ -3,12 +3,20 @@ window.addEventListener('DOMContentLoaded',init,false);
 function init() {
     var button = document.getElementsByClassName("button")
     button[0].addEventListener('click',HideSSB,false)
+    button[1].addEventListener('click',HideSSB2,false)
 }
 
 function HideSSB(){
     var SSB = document.getElementsByClassName('SSB')
     for (var i = 0, length = SSB.length; i < length; i++) 
     SSB[i].style.visibility='hidden';
+
+
+}
+function HideSSB2(){
+    var SSB2 = document.getElementsByClassName('SSBB')
+    for (var i = 0, length = SSB2.length; i < length; i++) 
+    SSBB[i].style.visibility='hidden';
 
 
 }
