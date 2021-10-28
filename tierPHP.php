c791af4aafb3f361e4bad7ccdfefe0dccf12a9f7
ebeshero <ebbondar@gmail.com>
2021-10-28 19:19:10 -0400

    removed extra table header row, b/c XQuery is outputting the whole table

--- tierPHP.php
+++ tierPHP.php
@@ -3,15 +3,6 @@
     <head><title>Super Smash Characters' Tiers</title></head>
     <body> 
         <h1>Characters' Tiers Based On Game</h1>
-        <table>
-            <tr> 
-                <th>Characters</th>  
-                <th>Game 1</th>
-                <th>Game 2</th>
-                <th>Game 3</th>
-                <th>Game 4</th>
-                <th>Game 5</th>
-            </tr>
             
         <?php
     require_once("config.php");
